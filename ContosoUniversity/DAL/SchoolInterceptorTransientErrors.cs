c876df4145913753987606e48c391613f820b636
Elizabeth Goodman <Elizabeth.Goodman@elliemae.com>
2018-01-18 14:38:14 -0800

    clean up using statements

--- ContosoUniversity/DAL/SchoolInterceptorTransientErrors.cs
+++ ContosoUniversity/DAL/SchoolInterceptorTransientErrors.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Data.Common;
-using System.Data.Entity;
 using System.Data.Entity.Infrastructure.Interception;
-using System.Data.Entity.SqlServer;
 using System.Data.SqlClient;
-using System.Diagnostics;
 using System.Reflection;
 using System.Linq;
 using ContosoUniversity.Logging;
