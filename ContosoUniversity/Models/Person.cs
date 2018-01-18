1aaf4e22f2d620ce26939c44431d62008a9e3580
Kha Nguyen <kha.nguyen@elliemae.com>
2018-01-18 14:40:55 -0800

    Person.cs model change

--- ContosoUniversity/Models/Person.cs
+++ ContosoUniversity/Models/Person.cs
@@ -25,5 +25,10 @@ namespace ContosoUniversity.Models
                 return LastName + ", " + FirstMidName;
             }
         }
+
+        [Required]
+        [StringLength(100)]
+        [Display(Name = "Email")]
+        public string Email { get; set;}
     }
 }
